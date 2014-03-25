146288a87edbe6b4dfb7be1e6867e18e7c8cbdbf
Jonathan Ross <jonathancharlesross@gmail.com>
2014-03-24 21:02:43 -0500

    Changing game dynamics slightly.

--- js/game_manager.js
+++ js/game_manager.js
@@ -72,7 +72,8 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.82 ? 1 : 3;
+    var rand = Math.random();
+    var value = rand < 0.80 ? 1 : rand < 0.90 ? 2 : 3;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
